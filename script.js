83456f655a02ad8608f92ab6cea2fa2aafa306a7
Ronan <ronan.lavorato@gmail.com>
2025-03-10 18:20:12 -0300

    sliders

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
         autoplay: true,
         autoplayTimeout: 3000,
         responsive: {
-            0: { items: 1 },
-            600: { items: 2 },
-            1000: { items: 3 }
+            0: { items: 1 }, //  Movil
+            600: { items: 2 }, // Tablet
+            1000: { items: 2} //  Pc
         }
     });
     $('.next').click(function() { owl.trigger('next.owl.carousel'); });
